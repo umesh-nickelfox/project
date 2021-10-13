a61e8e899a43c63cee7018291526f441084dbabb
kumarparth123 <itsaryan380@gmail.com>
2021-10-13 15:19:40 +0530

    initial commit

---
+++ src/components/typography/index.js
@@ -0,0 +1,6 @@
+
+export const Title = ({ children, style, weight = "regular", color, onClick }) => (
+    <h3 onClick={onClick} style={[weights[weight], { color }, style]}>
+      {children}
+    </h3>
+);
